63c388883c14659fb6ed3a16959cb886cf8c5564
Jon Agüera <jonaguera@gmail.com>
2012-07-14 13:05:05 +0200

    Ejercicio 3.3 completado

--- src/Jonaguera/AulaMentor/Ejercicio32Bundle/Controller/DefaultController.php
+++ src/Jonaguera/AulaMentor/Ejercicio32Bundle/Controller/DefaultController.php
@@ -4,12 +4,15 @@ namespace Jonaguera\AulaMentor\Ejercicio32Bundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+class DefaultController extends Controller {
 
-class DefaultController extends Controller
-{
-    
-    public function indexAction($name)
-    {
+    public function indexAction($name) {
+        //return new \Symfony\Component\HttpFoundation\Response('Hola Caracola');
+        return $this->render('JonagueraAulaMentorEjercicio32Bundle:Default:index.html.twig', array('name' => $name));
+    }
+
+    public function holaAction() {
         return new \Symfony\Component\HttpFoundation\Response('Hola Caracola');
     }
+
 }
